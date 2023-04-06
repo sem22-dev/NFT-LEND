41987f09494289bf60ad5b22b5933e8c5b131042
sem22-dev <thotjj22@gmail.com>
2023-04-06 22:00:53 +0530

    lll

---
+++ app/PreLoader.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import React, { useEffect } from "react";
+import { preLoaderAnim } from "./animations";
+import "./preloader.css"
+
+const PreLoader = () => {
+  useEffect(() => {
+    preLoaderAnim();
+  }, []);
+  return (
+    <div className="preloader">
+      <div className="texts-container"> 
+        <span>V</span>
+        <span>A</span>
+        <span>U</span>
+        <span>L</span>
+        <span>T</span>
+        <span>S</span>
+        <span>W</span>
+        <span>A</span>
+        <span>P</span>
+      </div>
+    </div>
+  );
+};
+
+export default PreLoader;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./Navbar"
 import Landing from "./Landing"
 import Process from "./Process/page"
+import PreLoader from "./PreLoader"
 
 export default function Home() {
   return (
     
        <main className="absolute top-[-10px] w-full">
+          <PreLoader />
            <Landing/>
            <Process />
        </main>
