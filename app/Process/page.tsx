691d3d577e8f83f255eb6e5ba0bc60ba9a4d6d2e
sem22-dev <thotjj22@gmail.com>
2023-04-07 23:25:32 +0530

    learn more

--- app/Process/page.tsx
+++ app/Process/page.tsx
@@ -99,50 +99,36 @@ export default function Process(){
                     </div>
                 </div>
 
-                                <a
-                className="mx-auto w-40 text-center group relative inline-flex items-center overflow-hidden rounded border border-current px-8 py-3 text-primary focus:outline-none focus:ring active:text-indigo-500"
-                href="/download"
+        <div className="w-400 mx-auto">
+                            <a
+            className=" group relative inline-flex items-center overflow-hidden rounded border border-current px-8 py-3 text-primary focus:outline-none focus:ring active:text-indigo-500"
+            href="/download"
+            >
+            <span
+                className="absolute left-0 -translate-x-full transition-transform group-hover:translate-x-4"
+            >
+                <svg
+                className="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
                 >
-                <span
-                    className="absolute left-0 -translate-x-full transition-transform group-hover:translate-x-4"
-                >
-                    <svg
-                    className="h-5 w-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M17 8l4 4m0 0l-4 4m4-4H3"
-                    />
-                    </svg>
-                </span>
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+                </svg>
+            </span>
 
-                <span className="text-sm font-medium transition-all group-hover:ml-4">
-                    Learn More
-                </span>
-                </a>
-
-
-
-
-                <div className="flex flex-col w-[100%] bg-[#080C14] h-[100%] lg:p-20 p-8">
-
-                <div className="flex flex-col items-center justify-center text-center">
-                        <div className="color-code-1 lg:text-[54px] text-[28px] lg:leading-[129px] leading-[62px] font-bold">
-                            <h1>Why Choose Us?</h1>
-                        </div>
-
-                        <div className="text-white lg:w-[800px] lg:text-[20px] w-[400px]">
-                            <p>We are passionate about enabling NFT holders to take advantages of the full potential of their digital assets, VaultSwap was created to provide a secure, fair, and transparent solution for NFT owners seeking financial opportunities</p>
-                        </div>
-                </div>
-                    </div>
-                    </div>
+            <span className="text-sm font-medium transition-all group-hover:ml-4">
+                Learn More
+            </span>
+            </a>
+        </div>
+</div>
         
         
     )
