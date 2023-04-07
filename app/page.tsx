c192ff6a5e74a8fb90ea3e5ed29ef2e11ff609c7
sem22-dev <thotjj22@gmail.com>
2023-04-07 16:17:47 +0530

    preload add

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     
        <main className="absolute top-[0px] w-full">
-          {/* <PreLoader /> */}
+          <PreLoader />
            <Landing/>
            <Process />
        </main>
