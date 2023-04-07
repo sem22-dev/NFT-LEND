31a1f3c40121b4df7cced658b1e3a4de957d26ae
sem22-dev <thotjj22@gmail.com>
2023-04-07 21:24:37 +0530

    how it works

--- app/Process/page.tsx
+++ app/Process/page.tsx
@@ -1,28 +1,111 @@
+// export default function Process(){
+//     return(
+//         <div className=" flex flex-col w-[100%] bg-gray-800 h-[100%] lg:p-20 p-0 sm:p-8">
+
+//             <div className="flex flex-col gap-10 items-center justify-center text-center">
+//                     <div className="color-code-1 lg:text-[54px] text-[28px] lg:leading-[129px] leading-[62px] font-bold">
+//                         <h1>The process is pretty simple</h1>
+//                     </div>
+
+//                     <div className="text-white lg:text-[32px]">
+//                         <p>ArtLend has emerged as the top-notch liquidity protocol for 
+//                         non-fungible tokens (NFTs), granting NFT holders the leverage to secure cryptocurrency loans from lenders by 
+//                         pledging their NFTs as collateral.</p>
+//                     </div>
+//             </div>
+
+
+//         <div className=" flex flex-row container p-20 gap-5 items-center justify-center">
+//             <div className="color-code-1 leading-10 text-[24px] cursor-pointer">
+//                 <h2>Borrowers</h2>
+//             </div>
+//             <div className="text-[24px] text-gray-500 cursor-pointer">
+//                 <h2>Lenders</h2>
+//             </div>
+            
+//         </div>
+//         </div>
+//     )
+// }
+
 export default function Process(){
     return(
-        <div className=" flex flex-col w-[100%] bg-gray-800 h-[100%] lg:p-20 p-8">
+        <div className="flex flex-col w-[100%] bg-[#080C14] h-[100%] lg:p-20 p-8">
 
-            <div className="flex flex-col gap-10 items-center justify-center text-center">
+            <div className="flex flex-col items-center justify-center text-center">
                     <div className="color-code-1 lg:text-[54px] text-[28px] lg:leading-[129px] leading-[62px] font-bold">
                         <h1>The process is pretty simple</h1>
                     </div>
 
-                    <div className="text-white lg:text-[32px]">
+                    <div className="text-white lg:w-[800px] lg:text-[32px] w-[400px]">
                         <p>ArtLend has emerged as the top-notch liquidity protocol for 
                         non-fungible tokens (NFTs), granting NFT holders the leverage to secure cryptocurrency loans from lenders by 
                         pledging their NFTs as collateral.</p>
                     </div>
             </div>
+            <div className="flex flex-row container p-20 gap-5 items-center justify-center">
+    <div className="color-code-1 leading-10 text-[24px] cursor-pointer">
+        <h2>Borrowers</h2>
+    </div>
+    <div className="text-[24px] text-gray-500 cursor-pointer flex items-center">
+        <h2>Lenders</h2>
+    </div>
 
+</div>
+<div className="text-white lg:text-[44px] text-[30px] lg:leading-[129px] leading-[62px] font-bold ml-11">
+    <h1>List your NFT</h1>
+</div>
+<p className="text-white text-left text-[25px] ml-11">
+    The process starts with listing your NFT <br></br> and defining the loan's terms.
+    <br></br> After that, other users will make loan offers to you
+</p>
+    <div className="flex flex-wrap justify-center">
+    <div className="w-6/12 sm:w-4/12 px-4">
+    <img src="/images/1.png" alt="..." className="shadow rounded max-w-full h-auto align-middle border-none mt-4" style={{maxWidth: '60%', height: 'auto', marginTop: '-70%', marginLeft:'70%'}} />
+  </div>
+</div>
+<div className="flex flex-wrap justify-center">
+    <div className="w-6/12 sm:w-4/12 px-4">
+        <img src="/images/2.png" alt="..." className="shadow rounded max-w-full h-auto align-middle border-none " style={{maxWidth: '60%', height: 'auto', marginLeft:'-30%', marginTop: '-10%'}} />
+    </div>
+    <div className="text-white lg:text-[44px] text-[30px] lg:leading-[129px] leading-[62px] font-bold ml-11">
+        <h1>Pick the best loan offer</h1>
+    </div>
+    <p className="text-white text-left text-[25px]" style={{ marginLeft: '42%',marginTop:'-20%' }}>
+        The process starts with listing your NFT <br></br> and defining the loan's terms.
+        <br></br> After that, other users will make loan offers to you
+    </p>
+</div>
+<div className="text-white lg:text-[44px] text-[30px] lg:leading-[129px] leading-[62px] font-bold ml-11">
+    <h1>Repay Loan, Get NFT back</h1>
+</div>
+<p className="text-white text-left text-[25px] ml-11">
+    When you repay your loan on <br></br> time, your NFT will be automatically
+    <br></br> returned to your wallet
+</p>
+    <div className="flex flex-wrap justify-center">
+    <div className="w-6/12 sm:w-4/12 px-4">
+    <img src="/images/3.png" alt="..." className="shadow rounded max-w-full h-auto align-middle border-none mt-4" style={{maxWidth: '60%', height: 'auto', marginTop: '-70%', marginLeft:'70%'}} />
+  </div>
+</div>
+<br>
+</br>
 
-        <div className="flex flex-row container p-20 gap-5 items-center justify-center">
-            <div className="color-code-1 leading-10 text-[24px] cursor-pointer">
-                <h2>Borrowers</h2>
-            </div>
-            <div className="text-[24px] text-gray-500 cursor-pointer">
-                <h2>Lenders</h2>
-            </div>
+<button type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Learn More</button>
+<div className="flex flex-col w-[100%] bg-[#080C14] h-[100%] lg:p-20 p-8">
+
+<div className="flex flex-col items-center justify-center text-center">
+        <div className="color-code-1 lg:text-[54px] text-[28px] lg:leading-[129px] leading-[62px] font-bold">
+            <h1>Why Choose Us?</h1>
+        </div>
+
+        <div className="text-white lg:w-[800px] lg:text-[20px] w-[400px]">
+            <p>We are passionate about enabling NFT holders to take advantages of the full potential of their digital assets, VaultSwap was created to provide a secure, fair, and transparent solution for NFT owners seeking financial opportunities</p>
+        </div>
+</div>
         </div>
         </div>
+        
+        
     )
 }
