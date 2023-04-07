235e1748fddaa64b976c68c03cee09af16bebd4f
sem22-dev <thotjj22@gmail.com>
2023-04-07 23:21:05 +0530

    learn more

--- app/Process/page.tsx
+++ app/Process/page.tsx
@@ -99,6 +99,33 @@ export default function Process(){
                     </div>
                 </div>
 
+                                <a
+                className="mx-auto w-40 text-center group relative inline-flex items-center overflow-hidden rounded border border-current px-8 py-3 text-primary focus:outline-none focus:ring active:text-indigo-500"
+                href="/download"
+                >
+                <span
+                    className="absolute left-0 -translate-x-full transition-transform group-hover:translate-x-4"
+                >
+                    <svg
+                    className="h-5 w-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    />
+                    </svg>
+                </span>
+
+                <span className="text-sm font-medium transition-all group-hover:ml-4">
+                    Learn More
+                </span>
+                </a>
 
 
 
