5d22c88c13196550842ca20e50d52cb5e7c0e151
sem22-dev <thotjj22@gmail.com>
2023-04-11 22:20:24 +0530

    removed anime in small-screens

--- app/LandingPage/Landing.tsx
+++ app/LandingPage/Landing.tsx
@@ -7,7 +7,7 @@ export default function Home(){
     return(
         
         <div>
-            <div className="pt-10 px-7 lg:px-16 bg-center bg-cover bg-no-repeat sm:max-w-full h-[500px] lg:h-[700px] bg-none " style={{ backgroundImage: "url('/images/backgroundImage3.png')" }}>
+            <div className="pt-10 px-7 hidden md:block lg:px-16 bg-center bg-cover bg-no-repeat sm:max-w-full h-[500px] lg:h-[700px] " style={{ backgroundImage: "url('/images/backgroundImage3.png')" }}>
                     <div className=" container flex flex-col gap-16 xl:gap-10 mx-auto ">
                         
                         <div className=" lg:text-[56px] text-[26px] font-bold text-center ">
@@ -24,16 +24,16 @@ export default function Home(){
             </div>
             
             {/* mobile */}
-            {/* <div className=" pt-10 md:hidden bg-center bg-cover bg-no-repeat max-w-full h-[370px]  " style={{ backgroundImage: "url('/images/backgroundImage3.png')" }}> 
+            <div className=" px-7 pt-10 md:hidden bg-center bg-cover bg-no-repeat max-w-full h-[370px] "> 
 
                 <div className="  mx-auto ">
     
-                    <div className="text-[16px] pb-16 w-full font-bold text-center">
-                            <p className='pb-10'>Use your NFT collection as collateral for a crypto loan</h1>
-                        <div className="flex flex-row  items-center justify-center gap-[100px]">
-                            <button className="w-[100px] h-[38px] text-white text-[12px]  bg-gray-800">Need a Loan</button>
+                    <div className="text-[26px] pb-16 font-bold text-center">
+                            <p className='pb-10'>Use your NFT collection as collateral for a crypto loan</p>
+                            <div className="font-semibold flex gap-2  flex-col text-[17px] items-center justify-center">
+                            <motion.button  whileHover={{ scale: 1.05 }} className="border-header w-40 px-3 py-5 sm:px-8  bg-bg text-white leading-[24.2px] rounded-sm tracking-wider">Need a Loan</motion.button>
 
-                            <button className="w-[110px] h-[38px] text-[12px] border-gray-800 bg-transparent border-2">Want to lend</button>
+                            <motion.button  whileHover={{ scale: 1.05 }} className=" bg-header px-3 w-40 py-5 sm:px-8 text-bg leading-[24.2px] rounded-sm tracking-wider">Want to lend?</motion.button>
                         </div>
                     </div>
 
@@ -41,7 +41,7 @@ export default function Home(){
        
                 </div>
 
-            </div> */}
+            </div>
                 
          </div>
                     
