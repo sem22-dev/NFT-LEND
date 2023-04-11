a20e5c294cbf82d01be381b31cb9bf1684ac8d8b
sem22-dev <thotjj22@gmail.com>
2023-04-11 15:44:49 +0530

    updates

--- app/preloader/PreLoader.tsx
+++ app/preloader/PreLoader.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useEffect } from "react";
-import { preLoaderAnim } from "./animations";
+import { preLoaderAnim } from "../animations";
 import "./preloader.css"
 
 const PreLoader = () => {
